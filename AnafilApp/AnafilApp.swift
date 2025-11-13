598692051b62d64334bd36ad25155e8fc2cd82b0
Elvis Alain Calzada Trinidad <elviscalzada@192.168.1.18>
2025-11-13 14:11:02 -0600

    Done with 'Notificación_DP Complete'

--- AnafilApp/AnafilApp.swift
+++ AnafilApp/AnafilApp.swift
@@ -20,7 +20,6 @@ struct AnafilApp: App {
 
 struct AnafilAppRoot: View {
     @AppStorage("isFirstLaunch") var isFirstLaunch: Bool = true // Bandera de la primera apertura (para que solo sea la primera vez)
-    @Environment(\.scenePhase) var scenePhase // Primer plano?
     @Query var users: [User] // Verificar perfil
     @Environment(\.modelContext) private var modelContext
     
@@ -35,10 +34,8 @@ struct AnafilAppRoot: View {
                 }
             }
         }
-        .onChange(of: scenePhase) { _, newPhase in
-            if newPhase == .active {
-                checarYEnviarNotificacion()
-            }
+        .onAppear {
+            checarYEnviarNotificacion()
         }
     }
     
@@ -46,26 +43,12 @@ struct AnafilAppRoot: View {
         guard let user = users.first else {return} // Existe el usuario?
         guard user.isProfileIncomplete() else {return} // Verificar si el perfil está incompleto
         
-        let ultimasNotis = user.historialAcciones // Tiempo de espera, por si el usuario sale y se mete constantemente
-            .filter { $0.tipo_accion == .profileUpdate}
-            .sorted { $0.fecha_hora > $1.fecha_hora}
-        let tiempoDeEspera: TimeInterval = 0 // Modificar timepo de espera
         
-        let debeEnviarNuevaNoti: Bool
-        if let ultimaNoti = ultimasNotis.first { // Si ya existe una, vemos si pasó el tiempo de espera
-            debeEnviarNuevaNoti = Date().timeIntervalSince(ultimaNoti.fecha_hora) > tiempoDeEspera
-        } else {
-            debeEnviarNuevaNoti = true // Si no hay notificaciones previas de este tipo, enviamos
-        }
-        
-        if debeEnviarNuevaNoti {
-            let nuevaNoti = HistorialAcciones(
-                tipo_accion: .profileUpdate,
-                detalle: "No olvides llenar los campos de Datos Personales, tu información es muy importante."
-            )
-            modelContext.insert(nuevaNoti)
-            user.historialAcciones.append(nuevaNoti)
-            print("Notificación de perfil incompleto generada al abrir la app.")
-        }
+        let nuevaNoti = HistorialAcciones(
+            tipo_accion: .profileUpdate,
+            detalle: "No olvides llenar los campos de Datos Personales, tu información es muy importante."
+        )
+        modelContext.insert(nuevaNoti)
+        user.historialAcciones.append(nuevaNoti)
     }
 }
